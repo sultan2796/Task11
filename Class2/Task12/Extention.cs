f5eeb460675706e4dbd4b86d0238bd677cb20a95
sultan2796 <sultantagiyev303@gmail.com>
2023-07-22 16:18:06 +0400

    Task send

---
+++ Class2/Task12/Extention.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    internal static class Extention
+    {
+        public static bool IsOdd( this int x)
+        {
+            if(x % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsEven( this int x)
+        {
+            if (x % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsContain(this string text)
+        {
+            foreach(char c in text)
+            {
+                return true;
+            }
+            return false;
+        }
+       
+        public static string ToCaptalize(this string text)
+        {
+            text = text.Replace(text[0], char.ToUpper(text[0]));
+            return text;
+        }
+
+        public static int[] GetValueIndex(this string text,char x)
+        {
+            int[] index = new int[text.Length];
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == x)
+                {
+                    index[i] = i;
+                }
+                else
+                {
+                    index[i] = -1;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ Class2/Task12/Program.cs
@@ -0,0 +1,28 @@
+﻿using Task12;
+
+int x = 35;
+int y = 56;
+int c = 67;
+string text = "salamaa";
+
+bool result = Extention.IsOdd(x);
+bool result2 = y.IsOdd();
+bool result3 = c.IsEven();
+bool result4 = text.IsContain();
+Console.WriteLine(result);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
+Console.WriteLine(result4);
+Console.WriteLine(text.ToCaptalize());
+
+foreach (var item in text.GetValueIndex('a'))
+{
+    if (item != -1)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+
+
